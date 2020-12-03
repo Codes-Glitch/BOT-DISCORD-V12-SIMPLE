061c06c0d9256083a5e109db3db718d8970f4abe
Glitch (earthy-ambitious-sesame) <none>
2020-12-03 08:51:07 +0000

    🐇⛑ Checkpoint ./src/Unicron.js:24282868/446

--- src/Unicron.js
+++ src/Unicron.js
@@ -6,5 +6,16 @@ const client = new Unicron();
     await client.registerItems('../items/');
     await client.registerCommands('../commands/');
     await client.registerEvents('../events/');
+  const status = [
+    `${client.guilds.cache.size} Server | ${client.users.cache.size} User `,
+    `Made By 𝙁𝘾 么 Glitch Editz#5631`
+    `to set status Go to file : src/Unicron.js`//can add another
+  ]
+  setInterval(() => {
+    client.user.setActivity(status[Math.floor(Math.random() * status.length)], {type:"WATCHING"})
+  }, 5000)
+  
+  
+  
     await client.login(process.env.BOT_TOKEN);
 })();
