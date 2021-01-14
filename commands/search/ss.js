58f7a208ace977fa72a331223fef9b0519b8792c
Glitch (open-dust-airship) <none>
2021-01-14 14:26:28 +0000

    🐉👶 Checkpoint ./commands/search/ss.js:26780611/1297

--- commands/search/ss.js
+++ commands/search/ss.js
@@ -18,16 +18,8 @@ module.exports = {
     
     const urls = args[0];
     if(!urls)return message.reply("```\nwhere is the link -_\n```")
-      if(!args[0] < 8) return message.reply('<a:failed:798526823976796161> https is too short to reach - 7 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-     if(!args[0] < 7) return message.reply('<a:failed:798526823976796161> https is too short to reach - 6 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-     if(!args[0] < 6) return message.reply('<a:failed:798526823976796161> https is too short to reach - 5 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-     if(!args[0] < 5) return message.reply('<a:failed:798526823976796161> https is too short to reach - 4 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-     if(!args[0] < 4) return message.reply('<a:failed:798526823976796161> https is too short to reach - 3 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-     if(!args[0] < 3) return message.reply('<a:failed:798526823976796161> https is too short to reach - 2 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-     if(!args[0] < 2) return message.reply('<a:failed:798526823976796161> https is too short to reach - 1 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-     if(!args[0] < 1) return message.reply('<a:failed:798526823976796161> https is too short to reach - 0 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-if(!args[0] < 9) return message.reply('<a:failed:798526823976796161> https is too short to reach - 8 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-
+      if(!urls < 8) return message.reply('<a:failed:798526823976796161> https is too short to reach - 8 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
+   
      
      
    
