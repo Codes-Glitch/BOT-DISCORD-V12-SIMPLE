76ec0a49d2dc82d29adf2cf1369fdcb28c0bf2fa
Glitch (open-dust-airship) <none>
2021-01-15 12:49:14 +0000

    💓👨‍👨‍👦 Checkpoint ./commands/search/invite-bot.js:26780611/4432

--- commands/search/invite-bot.js
+++ commands/search/invite-bot.js
@@ -5,38 +5,56 @@ module.exports = {
   description: "Get bot link invite",
   usage: "inbot",
   run: async (client, message, args, msss) => {
-   message.delete();
-  //  let annel = message.guild.channels.cache.find((x) => (x.name === ``))
- const db = require("quick.db");
-  const msgg = args.join(" ");
-  
-  let chnnel =  message.guild.channels.cache.find(x => x.id === db.get(`inbot`));
-  if (!msgg) return message.channel.send("<a:failed:798526823976796161> Please Give ID Bot And Prefix Bot");
-    const mss = args[1]
-    if(!mss) return message.channel.send("<a:failed:798526823976796161> Prefix Bot");
+    message.delete();
+    //  let annel = message.guild.channels.cache.find((x) => (x.name === ``))
+    const db = require("quick.db");
+    const msgg = args.join(" ");
 
-  await message.channel.send(`Check Channel ${chnnel || `<a:failed:798526823976796161> Failed to Send` }`)
-    
-   const ar = args
-    if (!msgg) return message.channel.send("<a:failed:798526823976796161> Please Give ID Bot");
-    if (msgg.length > 18) return message.channel.send("<a:failed:798526823976796161> Too Long ID - 18 Limit");
-    if (isNaN(msgg))return message.reply("<a:failed:798526823976796161> This is not an ID")  
-  //await message.channel.send("Check Channel")
- const me = message.author.tag
+    let chnnel = message.guild.channels.cache.find(
+      x => x.id === db.get(`inbot`)
+    );
+    if (!msgg)
+      return message.channel.send(
+        "<a:failed:798526823976796161> Please Give ID Bot And Prefix Bot"
+      );
+    const mss = args[1];
+    if (!mss)
+      return message.channel.send("<a:failed:798526823976796161> Prefix Bot");
+
+    const ar = args;
+    if (!msgg)
+      return message.channel.send(
+        "<a:failed:798526823976796161> Please Give ID Bot"
+      );
+    if (msgg.length > 25)
+      return message.channel.send(
+        "<a:failed:798526823976796161> Too Long ID - 18 Limit"
+      );
+ /*   if (isNaN(msgg))
+      return message.reply("<a:failed:798526823976796161> This is not an ID");
+  */  //await message.channel.send("Check Channel")
+    const me = message.author.tag;
     //return message.channel.send("Check Channel")
-   const { Discord, MessageEmbed} = require("discord.js");
+    const { Discord, MessageEmbed } = require("discord.js");
     //await message.channel.send("Check Channel")
     const no = new MessageEmbed()
-    . setTitle ("BOT LINK 
- return chnnel.send(`> INVITE BOT\n> [Clink di sini](https://discord.com/oauth2/authorize?client_id=${ar}&scope=bot&permissions=8)\n=> Author: \`\`\`css\n${me}\n\`\`\``).then(m => {
+      .setTitle("BOT LINK ID")
+      .setDescription(`Author: \`\`\`css\n${me}\n\`\`\``)
+. setColor ("YELLOW")
+      .addField(
+        i
+        "INVITE BOT",
+        `[Clink Here](https://discord.com/oauth2/authorize?client_id=${ar}&scope=bot&permissions=8)`
+      );
+    await message.channel.send(
+      `Check Channel ${chnnel ||
+        `<a:failed:798526823976796161> Failed to Send`}`
+    );
 
-      m.react("✅")
+    return chnnel.send(no).then(m => {
+      m.react("✅");
 
-      m.react("❌")
-
-    })
- 
-  
-     }}
-
-;
+      m.react("❌");
+    });
+  }
+};
