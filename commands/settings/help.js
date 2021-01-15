b327eba02f09497b9747b7a8fd1df7a95341a2d1
Glitch (open-dust-airship) <none>
2021-01-15 11:03:07 +0000

    ☄️💗 Checkpoint ./commands/settings/help.js:26780611/1684

--- commands/settings/help.js
+++ commands/settings/help.js
@@ -1,4 +1,4 @@
-const discord = require("discord.js")
+const { discord, Discord } = require("discord.js")
 const db = require("quick.db")
 
 module.exports = {
@@ -12,10 +12,11 @@ module.exports = {
       return message.channel.send("This command can only be used by owner").then(m=>m.delete({timeout:9000}).catch(e=>{}))
     }
         
- const [action, key, ...value] = args;
-if (action === 'inbot') {
- 
-    //ARGUMENT
+const [key, ...value] = args;
+        switch (key) {
+            case 'channel': { 
+              
+              //ARGUMENT
   let channel = message.mentions.channels.first()
   
     if(!channel) {
@@ -24,15 +25,34 @@ if (action === 'inbot') {
     
     //Now we gonna use quick.db
      db.set(`inbot`, channel.id)
-     
-    await message.channel.send(`<a:success:798526789114134548> message has been set channel ${channel}\nCommand is loading 10s`).then(m=>m.delete({timeout:10000}).catch(e=>{}))
-  switch (key) {
+       return message.channel.send(new Discord.MessageEmbed()
 
-                case '2': {
-               await message.channel.send("   
-                }
-  
+                    .setColor('RED')
+
+                    .setTimestamp()
+
+                    .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }) || client.user.displayAvatarURL({ dynamic: true }))
+
+                    .setDescription(await <a:success:798526789114134548> message has been set channel ${channel}\nCommand is loading 10s`
+   )
+    await message.channel.send(`<a:success:798526789114134548> message has been set channel ${channel}\nCommand is loading 10s`).then(m=>m.delete({timeout:10000}).catch(e=>{}))
+    }
+              case 'message': {
+                await message.channel.send("ah?")
+      }
+                
+                default:
+
+                return message.channel.send(new Discord.MessageEmbed()
+
+                    .setColor('RED')
+
+                    .setTimestamp()
+
+                    .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }) || client.user.displayAvatarURL({ dynamic: true }))
+
+                    .setDescription('Error: Invalid Key provided, Please try again.')            
     
-   }}
+   )}
   }
 }
