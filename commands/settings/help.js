66e9e43e380f2909e2cdb76dd1b8721032360b74
Glitch (open-dust-airship) <none>
2021-01-15 10:51:23 +0000

    💿👂 Checkpoint ./commands/settings/help.js:26780611/645

--- commands/settings/help.js
+++ commands/settings/help.js
@@ -11,30 +11,28 @@ module.exports = {
     if(!message.author.id === `${message.guild.ownerID}`) {
       return message.channel.send("This command can only be used by owner").then(m=>m.delete({timeout:9000}).catch(e=>{}))
     }
+        
+ const [action, key, ...value] = args;
+if (action === 'inbot') {
+ 
     //ARGUMENT
   let channel = message.mentions.channels.first()
-     
-  switch(args[1]){  
-
-   case "inbot":
-   
   
     if(!channel) {
       return message.channel.send("Please Mention the channel first")
     }
     
     //Now we gonna use quick.db
-  
- db.set(`inbot`, channel.id)
+     db.set(`inbot`, channel.id)
      
     await message.channel.send(`<a:success:798526789114134548> message has been set channel ${channel}\nCommand is loading 10s`).then(m=>m.delete({timeout:10000}).catch(e=>{}))
-  break ;
-   case "help":
-      
-      return message.channel.send("waht?")
+  switch (key) {
+
+                case '2': {
+               await message.channel.send("   
+                }
   
-  break ;
-  }  
     
+   }}
   }
 }
