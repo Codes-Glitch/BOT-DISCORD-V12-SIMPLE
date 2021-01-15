130d46d8e95690a47d811b3f2704752c317de0c3
Glitch (open-dust-airship) <none>
2021-01-15 13:00:37 +0000

    🍎🍙 Checkpoint ./commands/search/invite-bot.js:26780611/244

--- commands/search/invite-bot.js
+++ commands/search/invite-bot.js
@@ -8,7 +8,7 @@ module.exports = {
     message.delete();
     //  let annel = message.guild.channels.cache.find((x) => (x.name === ``))
     const db = require("quick.db");
-    const msgg = args.join(" ");
+    const msgg = args[0];
 
     let chnnel = message.guild.channels.cache.find(
       x => x.id === db.get(`inbot`)
@@ -19,9 +19,9 @@ module.exports = {
       );
     const mss = args[1];
     if (!mss)
-      return message.channel.send("<a:failed:798526823976796161> Prefix Bot");
+      return message.channel.send("<a:failed:798526823976796161> Please Give Prefix Bot");
 
-    const ar = args;
+  //  const ar = args;
     if (!msgg)
       return message.channel.send(
         "<a:failed:798526823976796161> Please Give ID Bot"
@@ -30,9 +30,9 @@ module.exports = {
       return message.channel.send(
         "<a:failed:798526823976796161> Too Long ID - 18 Limit"
       );
- /*   if (isNaN(msgg))
+    if (isNaN(msgg))
       return message.reply("<a:failed:798526823976796161> This is not an ID");
-  */  //await message.channel.send("Check Channel")
+    //await message.channel.send("Check Channel")
     const me = message.author.tag;
     //return message.channel.send("Check Channel")
     const { Discord, MessageEmbed } = require("discord.js");
@@ -42,9 +42,14 @@ module.exports = {
       .setDescription(`Author: \`\`\`css\n${me}\n\`\`\``)
 . setColor ("YELLOW")
       .addField(
-        i
+        "ID BOT",
+        `\`\`\`\n${msgg}\n\`\`\``)
+     .addField(
+        "PREFIX",
+        `\`\`\`\n${mss}\n\`\`\``)
+       .addField(
         "INVITE BOT",
-        `[Clink Here](https://discord.com/oauth2/authorize?client_id=${ar}&scope=bot&permissions=8)`
+        `[Clink Here](https://discord.com/oauth2/authorize?client_id=${msgg}&scope=bot&permissions=8)`
       );
     await message.channel.send(
       `Check Channel ${chnnel ||
