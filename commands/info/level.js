080b8cd32fc3e56c9f0f787c8adfc85f84f80ebe
Glitch (open-dust-airship) <none>
2021-01-16 12:38:19 +0000

    ⛳️🌎 Checkpoint ./commands/info/level.js:26780611/5987

--- commands/info/level.js
+++ commands/info/level.js
@@ -1,45 +1,38 @@
-const db = require('quick.db')
-const discord = require('discord.js')
-const { getInfo } = require("../../handlers/xp.js")
+const db = require("quick.db");
+const discord = require("discord.js");
+const { getInfo } = require("../../handlers/xp.js");
 module.exports = {
   name: "level",
   description: "Get the level of author or mentioned",
   usage: "level <user>",
   category: "info",
-  run: (client, message, args) => {
- 
-   message.delete();
+  run: (client, message, args, mass) => {
+    message.delete();
     const user = message.mentions.users.first() || message.author;
-       let chnnel = message.guild.channels.cache.find(
+    let chnnel = message.guild.channels.cache.find(
+      x => x.id === db.get(`loevel`)
+    );
+    if (user.id === client.user.id) {
+      //IF BOT
+      return message.channel.send("😉 | I am on level 500");
+    }
 
-      x => x.id === db.get(`level`)
-)
-    
-    if(user.id === client.user.id) { //IF BOT
-      return message.channel.send("😉 | I am on level 500")
+    if (user.bot) {
+      return message.channel.send("Bot do not have levels");
     }
-    
-    if(user.bot) {
-      return message.channel.send("Bot do not have levels")
-    }
-    
+
     let xp = db.get(`xp_${user.id}_${message.guild.id}`) || 0;
-    
-    const {level, remxp, levelxp} = getInfo(xp);
-    if(xp === 0) return message.channel.send(`**${user.tag}** is out of the xp`)
-    
-    let embed = new discord.MessageEmbed()
-    .setAuthor(user.username, message.guild.iconURL())
-    .setColor("#ff2050")
-    .setThumbnail(user.avatarURL())
-    .setDescription(`**LEVEL** - ${level}
-**XP** - ${remxp}/${levelxp}`)
-// await message.channel.send("h
-      
-    return chnnel.send(embed).then(m => {
-      m.react("✅");
 
-      m.react("❌");
-    });
-  }
-};
+    const { level, remxp, levelxp } = getInfo(xp);
+    if (xp === 0)
+      return message.channel.send(`**${user.tag}** is out of the xp`);
+  
+
+  const embed = new discord.MessageEmbed()
+      .setAuthor(user.username, message.guild.iconURL())
+      .setColor("#ff2050")
+      .setThumbnail(user.avatarURL()).setDescription(`**LEVEL** - ${level}
+**XP** - ${remxp}/${levelxp}`)
+  
+  
+  }}
