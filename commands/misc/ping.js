61caa3f8677c778648be6762353441dfa24f6321
Glitch (open-dust-airship) <none>
2021-01-16 17:18:05 +0000

    💎☃️ Checkpoint ./commands/misc/ping.js:26780611/1101

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -14,7 +14,37 @@ module.exports = {
     message.channel.send(em).then(m => 
                                   { m.react("✅")
                                     m.react("🔒")
-                                   ({timeout: 7000})});
+                                    ({timeout: 7000})});
+      client.on("messageReactionAdd", async (reaction, user) => {
+
+  if (user.bot) return; // If the user was a bot, return.
+
+ if (!reaction.message.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
+
+      if (reaction.emoji.name === "🔒") {
+
+         await  message.channel.bulkDelete(1);
+return message.channel.send("message Locked\nClick 🔓 to return the ping message").then(m => {
+
+      m.react("🔓")
+
+        client.on("messageReactionAdd", async (reaction, user) => {
+
+  if (user.bot) return; // If the user was a bot, return.
+
+ if (!reaction.message.guild) return; // If the user was reacting something but not in the guild/server, ignore them.
+
+      if (reaction.emoji.name === "🔓") {
+  await  message.channel.bulkDelete(1);
+            return message.channel.send(em).then(m=>m.delete({timeout:5000}).catch(e=>{}))
+    }}) 
+
+ 
+    })
+    }}) 
+
+ 
   }
+  
   
 }
