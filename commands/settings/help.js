71a506afc844731514ed62b23451ebf967039726
Glitch (open-dust-airship) <none>
2021-01-18 10:55:21 +0000

    ⛷🏯 Checkpoint ./commands/settings/help.js:26780611/9815

--- commands/settings/help.js
+++ commands/settings/help.js
@@ -64,8 +64,9 @@ module.exports = {
         break;
       case "help":
         {
-          return message.channel.send(e
-        /*    new MessageEmbed()
+          return message.channel.send(
+            e
+            /*    new MessageEmbed()
               .setTitle("Setting MSG")
               .setColor("GREEN")
               .addField("Set <key>", "\ninbot, level, utime")
@@ -121,25 +122,31 @@ module.exports = {
 
         let days = Math.floor(hours / 24);
 
+        let month = Math.floor(days / 12);
+
         seconds %= 60;
 
         minutes %= 60;
 
         hours %= 24;
-       const u =  message.mentions.members.first()
-    const us = message.guild.members.fetch(client.user.id)
- const ddi = `790937024941129759`
+
+        month %= 12;
+
+        const u = message.mentions.members.first();
+        const us = message.guild.members.fetch(client.user.id);
+        const ddi = `790937024941129759`;
         const uptime = new MessageEmbed()
-          .setTitle("UPTIME BOT ONLINE")
+          .setTitle(
+            "<:slowmode:799034410799595590> UPTIME BOT ONLINE <:slowmode:799034410799595590>"
+          )
           .setDescription(
-            `<a:success:798526789114134548> <@${ddi}> : \`${days} day(s),${hours} hours, ${minutes} minutes, ${seconds} seconds\``
+            `<a:success:798526789114134548> <@${ddi}> : \`${month} Month,${days} day(s),${hours} hours, ${minutes} minutes, ${seconds} seconds\``
           )
 
           .setColor("GREEN")
           .setTimestamp();
-        return message.channel
-          .send(uptime)
-          //.then(m => m.delete({ timeout: 10000 }).catch(e => {}));
+        return message.channel.send(uptime);
+        //.then(m => m.delete({ timeout: 10000 }).catch(e => {}));
       }
     }
   }
