9c88095d6e73394fc803c0c4b99cc6610793f903
Glitch (open-dust-airship) <none>
2021-01-18 10:32:06 +0000

    🐚🙌 Checkpoint ./commands/settings/help.js:26780611/8886

--- commands/settings/help.js
+++ commands/settings/help.js
@@ -9,128 +9,138 @@ module.exports = {
   category: "settings",
   run: async (client, message, args) => {
     //OWNER ONLY COMMAND
-    message.delete()
+    message.delete();
     if (!message.author.id === `${message.guild.ownerID}`) {
       return message.channel
         .send("This command can only be used by owner")
         .then(m => m.delete({ timeout: 9000 }).catch(e => {}));
     }
     let e = new MessageEmbed()
-    .setTitle ("Setting MSG")
-    . setColor ("GREEN")
-    .addField ("Set <key>","\ninbot, level")
-    .setTimestamp ()
-       .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }) || client.user.displayAvatarURL({ dynamic: true }))
-    const w = args.join(" ")
- if (!w) return message.channel.send(e);
+      .setTitle("Setting MSG")
+      .setColor("GREEN")
+      .addField("Set <key>", "\ninbot, level")
+      .setTimestamp()
+      .setFooter(
+        message.author.tag,
+        message.author.displayAvatarURL({ dynamic: true }) ||
+          client.user.displayAvatarURL({ dynamic: true })
+      );
+    const w = args.join(" ");
+    if (!w) return message.channel.send(e);
 
     const [key, ...value] = args;
     switch (key) {
-      case "inbot": {
-        //ARGUMENT
-        let channel = message.mentions.channels.first();
+      case "inbot":
+        {
+          //ARGUMENT
+          let channel = message.mentions.channels.first();
 
-        if (!channel) {
-          const wwww = new MessageEmbed()
-         . setTitle("Settings Message")
-         . setDescription("<a:failed:798526823976796161> Please Mention the channel first")
-         . setColor ("GREEN")
-         .setTimestamp()
-       return message.channel.send(wwww).then(m=>m.delete({timeout:12000}).catch(e=>{}))
-         }
-
-        //Now we gonna use quick.db
-        db.set(`inbot_${message.guild.id}`, channel.id);
-const www = new MessageEmbed()
-.setTitle ("Settings Message")
-. setDescription (`<a:success:798526789114134548> message has been set channel ${channel}`
-          )
-. setColor ("GREEN")
-.setTimestamp()
-           await message.channel
-          .send(www)
-          .then(m => m.delete({ timeout: 10000 }).catch(e => {}));
-      }
-        break
-        case "help" : {
-          return message.channel.send(new MessageEmbed()
-                                      .setTitle ("Setting MSG")
-    . setColor ("GREEN")
-    .addField ("Set <key>","\ninbot, level")
-    .setTimestamp ()
-       .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }) || client.user.displayAvatarURL({ dynamic: true }))
-    
-                                      )
+          if (!channel) {
+            const wwww = new MessageEmbed()
+              .setTitle("Settings Message")
+              .setDescription(
+                "<a:failed:798526823976796161> Please Mention the channel first"
+              )
+              .setColor("GREEN")
+              .setTimestamp();
+            return message.channel
+              .send(wwww)
+              .then(m => m.delete({ timeout: 12000 }).catch(e => {}));
           }
+
+          //Now we gonna use quick.db
+          db.set(`inbot_${message.guild.id}`, channel.id);
+          const www = new MessageEmbed()
+            .setTitle("Settings Message")
+            .setDescription(
+              `<a:success:798526789114134548> message has been set channel ${channel}`
+            )
+            .setColor("GREEN")
+            .setTimestamp();
+          await message.channel
+            .send(www)
+            .then(m => m.delete({ timeout: 10000 }).catch(e => {}));
+        }
         break;
-          case "level": {
-        //ARGUMENT
-        let channel = message.mentions.channels.first();
+      case "help":
+        {
+          return message.channel.send(
+            new MessageEmbed()
+              .setTitle("Setting MSG")
+              .setColor("GREEN")
+              .addField("Set <key>", "\ninbot, level")
+              .setTimestamp()
+              .setFooter(
+                message.author.tag,
+                message.author.displayAvatarURL({ dynamic: true }) ||
+                  client.user.displayAvatarURL({ dynamic: true })
+              )
+          );
+        }
+        break;
+      case "level":
+        {
+          //ARGUMENT
+          let channel = message.mentions.channels.first();
 
-        if (!channel) {
-          const wwww = new MessageEmbed()
-         . setTitle("Settings Message")
-         . setDescription("<a:failed:798526823976796161> Please Mention the channel first")
-         . setColor ("GREEN")
-         .setTimestamp()
-       return message.channel.send(wwww).then(m=>m.delete({timeout:12000}).catch(e=>{}))
-         }
+          if (!channel) {
+            const wwww = new MessageEmbed()
+              .setTitle("Settings Message")
+              .setDescription(
+                "<a:failed:798526823976796161> Please Mention the channel first"
+              )
+              .setColor("GREEN")
+              .setTimestamp();
+            return message.channel
+              .send(wwww)
+              .then(m => m.delete({ timeout: 12000 }).catch(e => {}));
+          }
 
-        //Now we gonna use quick.db
-        db.set(`level_${message.guild.id}`, channel.id);
-const www = new MessageEmbed()
-.setTitle ("Settings Message")
-. setDescription (`<a:success:798526789114134548> message has been set channel ${channel}`
-          )
-. setColor ("GREEN")
-.setTimestamp()
-           await message.channel
-          .send(www)
-          .then(m => m.delete({ timeout: 10000 }).catch(e => {}))
-  };
-    
-    
-     break;
-    
+          //Now we gonna use quick.db
+          db.set(`level_${message.guild.id}`, channel.id);
+          const www = new MessageEmbed()
+            .setTitle("Settings Message")
+            .setDescription(
+              `<a:success:798526789114134548> message has been set channel ${channel}`
+            )
+            .setColor("GREEN")
+            .setTimestamp();
+          await message.channel
+            .send(www)
+            .then(m => m.delete({ timeout: 10000 }).catch(e => {}));
+        }
+
+        break;
+
       case "uptime": {
+        let seconds = Math.floor(message.client.uptime / 1000);
 
-          let seconds = Math.floor(message.client.uptime / 1000);
+        let minutes = Math.floor(seconds / 60);
 
-    let minutes = Math.floor(seconds / 60);
+        let hours = Math.floor(minutes / 60);
 
-    let hours = Math.floor(minutes / 60);
+        let days = Math.floor(hours / 24);
 
-    let days = Math.floor(hours / 24);
+        seconds %= 60;
 
-    seconds %= 60;
+        minutes %= 60;
 
-    minutes %= 60;
+        hours %= 24;
+       const u =  message.mentions.members.first()
+    const us = message.guild.members.fetch(`<@${u}>`)
+ 
+        const uptime = new MessageEmbed()
+          .setTitle("Settings Message")
+          .setDescription(
+            `<a:success:798526789114134548> ${u}: \`${days} day(s),${hours} hours, ${minutes} minutes, ${seconds} seconds\``
+          )
 
-    hours %= 24;
-
-    return message
-const uptime = new MessageEmbed()
-.setTitle ("Settings Message")
-. setDescription (`<a:success:798526789114134548> message has been set channel ${channel}`
-
-. setColor ("GREEN")
-.setTimestamp()
-           await message.channel
-          .send(www)
-          .then(m => m.delete({ timeout: 10000 }).catch(e => {}))
-  };
-    
-
-   
-      .catch(console.error);
-
-  
-
-};
-
-};
-
- }
-
+          .setColor("GREEN")
+          .setTimestamp();
+        return message.channel
+          .send(uptime)
+          //.then(m => m.delete({ timeout: 10000 }).catch(e => {}));
+      }
+    }
   }
-  
+};
