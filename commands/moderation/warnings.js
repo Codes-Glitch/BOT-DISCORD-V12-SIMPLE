b58c521b192d47a075d2c6df885961210fe86617
Glitch (open-dust-airship) <none>
2021-01-20 11:20:00 +0000

    🏘🌞 Checkpoint ./commands/moderation/warnings.js:26780611/630 ./reset-warns.js:26780611/1314

--- commands/moderation/warnings.js
+++ commands/moderation/warnings.js
@@ -1,21 +1,22 @@
 const db = require("quick.db");
 const MessageEmbed = require("discord.js");
+const DISCORD = require("discord.js");
 module.exports = {
   name: "warnings",
   description: "Get the warnings of yours or mentioned person",
   category: "moderation",
   run: (client, message, args) => {
     const user = message.mentions.members.first() || message.author;
-
+    const u = args[0];
+    if (!u)
+      return message.channel.send("warnings <@user>")
     let warnings = db.get(`warnings_${message.guild.id}_${user.id}`);
 
     if (warnings === null) warnings = 0;
 
-    message.channel.send(
-      new MessageEmbed()
-      . setTitle ("MODERATION BOT")
-      .setDescription(`<a:right:798696415089262632> ${user}\nThe number of members in the Warn: **${warnings}** warning`)
-      
-    );
-  }
+      message.channel.send(new DISCORD.MessageEmbed()
+        .setTitle("MODERATION WARN")
+         . setDescription (`<a:right:798696415089262632> ${user}\nThe number of members in the Warn: \n<a:right:798696415089262632> Warn:**${warnings}**`)
+      .
+   )}
 };
