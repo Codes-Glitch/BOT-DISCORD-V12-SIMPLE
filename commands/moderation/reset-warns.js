5e096c7f0e2809b3dd056768e159275fed483f1c
Glitch (open-dust-airship) <none>
2021-01-20 11:55:02 +0000

    🐞👷 Checkpoint ./commands/misc/say.js:26780611/81 ./commands/moderation/reset-warns.js:26780611/32 ./commands/search/ss.js:26780611/86

--- commands/moderation/reset-warns.js
+++ commands/moderation/reset-warns.js
@@ -4,6 +4,7 @@ module.exports = {
   name: "resetwarns",
   aliases: ["rwarns"],
   usage: "rwarns <@user>",
+  category: "moderation",
   description: "Reset warnings of mentioned person",
   run: async (client, message, args) => {
     
