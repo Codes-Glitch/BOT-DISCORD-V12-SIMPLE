c2940bc578ad8dc5327a469d8c915e90fcbf0865
Glitch (open-dust-airship) <none>
2021-01-20 02:43:19 +0000

    💁🖼 Checkpoint ./commands/misc/d.js:26780611/637

--- commands/misc/d.js
+++ commands/misc/d.js
@@ -1,22 +1,24 @@
 const db = require("quick.db");
 const discord = require ("discord.js")
 const toHex = require("colornames");
-n: "Get bot ping :/",
-
-  usage: "",
-
-  run: async (client, message, args) => {
-   message.delete();
+module.exports = {
+  name: "embedsay",
+  category: "misc",
+  description: "Get embed :/",
+  usage: "embedsay <msg> <color>",
+  run: (client, message, del, args) => {
+    message.delete();
    let embedtext = args.slice(0).join(" ")
           if(!embedtext) return message.channel.send("e[0]t")
+const regex = !/[^a-zA-Z0-9]+/g.test(name)
+const colo = args[1]
 
            let embed = new discord.MessageEmbed()
-           .setDescript
-const regex = !/[^a-zA-Z0-9]+/g.test(name)
-const colo = args[1]on(embedtext)
-           message.channel.send(embed);
+           . setDescription (embedtext)
+     . setColor (`${regex}${colo}`)
+                message.channel.send(embed);
 
        }
 }
 
-           . setColor (regex(args[1])````${}${colo}
+      
