1730b114bc14956d9809b7751b6b7f2250ef64ad
Glitch (open-dust-airship) <none>
2021-01-21 13:20:54 +0000

    🐦🚿 Checkpoint ./commands/search/ss.js:26780611/3736

--- commands/search/ss.js
+++ commands/search/ss.js
@@ -14,22 +14,26 @@ module.exports = {
   description: "Takes a screenshot of any webpage.",
   usage: "screenshot <URL>",
   run: async (client, message, args) => {
-    message.delete()
-    const = 
+    message.delete();
+    const user = message.author.tag
     const urls = args[0];
-    if(!urls)return message.channel.send(`\`\`\`\nwhere is the link -_\n\`\`\``)
-      if (urls.length < 8)
-    return message.reply('<a:failed:798526823976796161> https is too short to reach - 8 limit').then(m=>m.delete({timeout:9000}).catch(e=>{}))
-   
-     
-     
-   
+    if (!urls)
+      return message.channel
+        .send(`\`\`\`\n${user},where is the link -_\n\`\`\``)
+        .then(m => m.delete({ timeout: 5000 }).catch(e => {}));
+    if (urls.length < 8)
+      return message
+        .reply(
+          "<a:failed:798526823976796161> https is too short to reach - 8 limit"
+        )
+        .then(m => m.delete({ timeout: 9000 }).catch(e => {}));
+
     const site = /^(https?:\/\/)/i.test(urls) ? urls : `http://${urls}`;
     try {
       const { body } = await fetch(
         `https://image.thum.io/get/width/1920/crop/675/noanimate/${site}`
       );
-      
+
       return message.channel.send(
         `\`\`\`\nHere is a screenshot from requested URL\n\`\`\``,
         {
@@ -38,10 +42,12 @@ module.exports = {
       );
     } catch (err) {
       if (err.status === 404)
-        return message.channel.send("Could not find any results. Invalid URL?");
-      return message.reply(
-        `Oh no, an error occurred: \`${err.message}\`. Try again later!`
-      );
+        return message.channel
+          .send("Could not find any results. Invalid URL?")
+          .then(m => m.delete({ timeout: 14000 }).catch(e => {}));
+      return message
+        .reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`)
+        .then(m => m.delete({ timeout: 13000 }).catch(e => {}));
     }
   }
 };
