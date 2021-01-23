43c4e688e2adf4dacf1aeee67f4523d9829affc6
Glitch (open-dust-airship) <none>
2021-01-23 12:25:34 +0000

    🌅🎤 Checkpoint ./commands/info/codeskkwkw.js:26780611/2385

--- commands/info/codeskkwkw.js
+++ commands/info/codeskkwkw.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
 module.exports = {
-
   name: "code",
 
   description: "Display the code of the specified command.",
@@ -11,65 +10,41 @@ module.exports = {
   category: "info",
 
   run: (client, message, args, mass) => {
- 
+    message.delete();
     let code;
 
     try {
-
-      code = fs.readFileSync(`commands/${args[0]}.js`).toString()
+      code = fs.readFileSync(`commands/${args[0]}.js`).toString();
     } catch (error) {
-
       return message.channel.send(
-
         `I couldn't find a command called \`${args[0] || "Invalid text"}\`\n
         How to use Yes it is category/name.js
 `
-
       );
-
     }
 
     try {
-
       if (args[0]) {
-
         const options = {
-
           method: "POST",
 
           body: code,
 
           headers: {
-
-            "Content-Type": "application/json",
-
-          },
-
+            "Content-Type": "application/json"
+          }
         };
 
         message.channel.send(
-
-          `Here is the code for the ${
-
+          `Here is the code for the \`${
             args[0]
-
-          } command:\n\`\`\`js\n${code.substr(0, 1900)}\`\`\``
-
+          }\` command:\n\`\`\`js\n${code.substr(0, 992883)}\`\`\``
         );
-
       }
-
     } catch (e) {
-
       return message.channel.send(
-
         "There was an error displaying the command's code."
-
       );
-
     }
-
-  },
-
+  }
 };
-
