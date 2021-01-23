8d61c67bd5cf5899eb1e46741b37ba20798d0397
Glitch (open-dust-airship) <none>
2021-01-23 07:34:42 +0000

    🌔😚 Checkpoint ./server.js:26780611/46 ./commands/settings/help.js:26780611/1300 ./commands/moderation/prefix.js:26780611/1978

--- commands/settings/help.js
+++ commands/settings/help.js
@@ -206,52 +206,7 @@ module.exports = {
 
         }
     }*/
-        break;
-      case "prefix": {
-        if (!message.member.hasPermission("MANAGE_GUILD"))
-          return message.channel.send(
-            "You Don't Have Enough Permission To Execute This Command - Manage Server"
-          );
-
-        const Color = "GREEN";
-
-        let Prefix = await db.fetch(`refix_${message.guild.id}`);
-
-        if (!Prefix) Prefix = Default_Prefix;
-
-        const NewPrefix = args.join(" ");
-
-        if (!NewPrefix)
-          return message.channel.send("Please Give New Prefix Of Bot!");
-
-        if (NewPrefix.length > 10)
-          return message.channel.send("Too Long Prefix - 10 Limit");
-
-        if (NewPrefix === Prefix)
-          return message.channel.send("Given Prefix Is The Current Prefix!");
-
-        const Emb = new MessageEmbed()
-
-          .setColor(Color || "RANDOM")
-
-          .setTitle("Sucess")
-
-          .setDescription(`New Prefix Has Been Setted - ${NewPrefix}`)
-
-          .setFooter(`Setted By ${message.author.username}`)
-
-          .setTimestamp();
-
-        await db.set(`Prefix_${message.guild.id}`, NewPrefix);
-
-        try {
-          return message.channel.send(Emb);
-        } catch (error) {
-          return message.channel.send(
-            `New Prefix Has Been Setted - ${NewPrefix}`
-          );
-        }
-      }
+      
     }
   }
 };
